826b532ce750271d123df9b96ad53619a9de5c1c
ItsPablo99 <pablobrias3@gmail.com>
2023-05-25 14:49:57 +0200

    Cambios al conexion.php

--- Web/recursos/php/conexion.php
+++ Web/recursos/php/conexion.php
@@ -1,7 +1,7 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
+	$host = "host";
+	$user = "user";
+	$pass = "password";
 	try {
 		$bbdd = new PDO("mysql:host=$host;dbname=tiempo", $user, $pass, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		$bbdd -> exec ("set names utf8");
